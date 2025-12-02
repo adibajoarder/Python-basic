8379a1d2229c1d15caf2baf1b317fc8d2e795336
Adiba <adibajoarder>
2025-12-02 15:54:04 +0600

    initial commit

---
+++ test.py
@@ -0,0 +1,94 @@
+#txt = "The best things in life are free!"
+#print("free" in txt)
+#b = "Hello, World!"
+#print(b[-5:-2])
+#a = "Hello, world!"
+#print(a.replace("H", "j"))
+#a = 200
+#b = 33
+#if b > a:
+ #   print("b is greater than a")
+#else:
+ #   print("b is not greater than a")
+#thistuple = ("apple", "banana", "cherry")
+#y = list(thistuple)
+#y.append("orange")
+#thistuple = tuple(y)
+#print(thistuple)
+#x = ("apple", "banana", "cherry")
+#y = list(x)
+#y[1] = "kiwi"
+#x = tuple(y)
+#print(x)
+#y.remove("apple")
+#thistuple = tuple(y)
+#print(x)
+#x = 5
+#print(not(x > 3 and x < 10))
+#thisdict = dict(name = "John", age = 36, country = "Norway")
+#print(thisdict)
+#car = {
+#"brand": "Ford",
+#"model": "Mustang",
+#"year": 1964
+#}
+#car.update({"year": 2020})
+
+#x = car.values()
+
+#print(x) 
+
+#car["color"] = "red"
+
+#print(x)
+#i = 1
+#while i < 6:
+  #print(i)
+  #i += 1
+#else:
+  #print("i is no longer less than 6")
+#import math
+
+#x = math.sqrt(64)
+
+#print(x)
+#price = 49
+#txt = f"It is very {'Expensive' if price>50 else 'Cheap'}"
+
+#print(txt)
+# print("Enter your name:")
+# name = input()
+# print(f"Hello {name}")
+#exit() 
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name 
+#         self.age = age
+#         # print("hello")
+        
+# p1 = Person("Adiba", 22)
+
+
+#         # return p1.name
+#         # return p1.age
+
+# print(p1.name)
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.__age = age
+
+# p1 = Person("Adiba", 22)
+
+# print(p1._Person__age)
+# marks = 75
+# if marks >= 80:
+#     print("A grade")
+# elif marks >= 70:
+#     print("B grade")
+# else : print("C grade")    
+
+age = 18 
+if age >= 18:
+ print("you are an adult")
+else: print("you are a minor")
